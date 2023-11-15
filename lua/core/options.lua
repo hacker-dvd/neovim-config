f0a12b79c67cd3fc5c9aac62242fe6970acbc1be
lc <2981228126@qq.com>
2023-11-15 20:05:24 +0800

    commit 1

---
+++ lua/core/options.lua
@@ -0,0 +1,14 @@
+local opt = vim.opt
+
+-- 行号
+opt.relativenumber = true
+opt.number = true
+
+-- 缩进
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+# 禁止自动换行
+opt.wrap = false
