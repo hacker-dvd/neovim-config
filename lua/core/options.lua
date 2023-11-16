d7c59cb416438b70bae9b4d0618ad2882c2b7c1d
lc <2981228126@qq.com>
2023-11-16 13:10:21 +0800

    commit 2

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -10,5 +10,23 @@ opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
-# 禁止自动换行
+-- 禁止自动换行
 opt.wrap = false
+
+-- 系统剪切板支持
+opt.clipboard:append("unnamedplus")
+
+-- 默认新窗口右和下
+opt.splitright = true
+opt.splitbelow = true
+
+-- 搜索
+-- 忽略大小写
+opt.ignorecase = true
+-- 智能判断，当输入的全是小写字母，大小写不敏感，当输入有大写字母时，大小写敏感
+opt.smartcase = true
+
+-- 启用真彩色
+-- opt.termguicolors = true
+-- opt.signcolumn = "yes"
+
