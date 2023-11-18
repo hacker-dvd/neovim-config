6f8788f538139c75c3a545096bb0c1bb2a3b28c5
lc <2981228126@qq.com>
2023-11-18 12:11:03 +0800

    paste

---
+++ lua/plugins/vim-oscyank.lua
@@ -0,0 +1,8 @@
+return {
+  "ojroques/vim-oscyank",
+  config = function()
+    vim.keymap.set('n', '<leader>c', '<Plug>OSCYankOperator')
+    vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
+    vim.keymap.set('v', '<leader>c', '<Plug>OSCYankVisual')
+  end,
+}
